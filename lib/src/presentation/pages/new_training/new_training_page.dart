613a8f9f9050d59e8bdff51eb58ded4ca835e2b8
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-12-26 13:37:37 -0300

    feat: update appbar using on new training

--- lib/src/presentation/pages/new_training/new_training_page.dart
+++ lib/src/presentation/pages/new_training/new_training_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_gym_track/src/presentation/widgets/custom_app_bar.dart';
+import 'package:my_gym_track/src/application/theme/custom_colors.dart';
+import 'package:my_gym_track/src/presentation/pages/new_training/widgets/new_training_sheet_widget.dart';
 
 class NewTrainingPage extends StatefulWidget {
   const NewTrainingPage({Key? key}) : super(key: key);
@@ -12,13 +13,39 @@ class _NewTrainingPageState extends State<NewTrainingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: CustomColors.primaryGreen,
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        backgroundColor: CustomColors.white,
+        title: const Text("Novo treino"),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_ios),
+          color: CustomColors.black,
+          iconSize: 30,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.save_outlined),
+            color: CustomColors.black,
+            iconSize: 30,
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
-            SizedBox(height: 32),
-            CustomAppBar(title: "Novo treino"),
+            SizedBox(height: 16),
+            NewTrainingSheetWidget(title: 'true'),
           ],
         ),
       ),
