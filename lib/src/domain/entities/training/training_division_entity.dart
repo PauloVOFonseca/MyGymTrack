292ee8f319734f44971a279dd19df66585280208
PauloVOFonseca <paulovitoradsifba@gmail.com>
2023-01-10 19:58:18 -0300

    feat: remove notes on training division entity

--- lib/src/domain/entities/training/training_division_entity.dart
+++ lib/src/domain/entities/training/training_division_entity.dart
@@ -3,11 +3,9 @@ import 'package:my_gym_track/src/domain/entities/exercise/exercise_entity.dart';
 class TrainingDivisionEntity {
   final String name;
   final List<ExerciseEntity> exercises;
-  final String? note;
 
   TrainingDivisionEntity({
     required this.name,
     required this.exercises,
-    this.note,
   });
 }
