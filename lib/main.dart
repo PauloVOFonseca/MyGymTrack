49e81f48a02fa9151eab6dfd3fb9afc28397415e
PauloVOFonseca <paulovitoradsifba@gmail.com>
2023-01-16 16:05:35 -0300

    feat: separate controller

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:my_gym_track/src/application/services/locator.dart';
 import 'package:my_gym_track/src/presentation/app/app_widget.dart';
 import 'package:my_gym_track/src/presentation/pages/all_exercises/all_exercises_controller.dart';
 import 'package:my_gym_track/src/presentation/pages/new_training/new_training_controller.dart';
+import 'package:my_gym_track/src/presentation/pages/new_training/training_division/training_division_controller.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -14,6 +15,8 @@ void main() {
             create: (_) => AllExercisesController()),
         ListenableProvider<NewTrainingController>(
             create: (_) => NewTrainingController()),
+            ListenableProvider<TrainingDivisionController>(
+            create: (_) => TrainingDivisionController()),
       ],
       child: const AppWidget(),
     ),
---
+++ lib/src/presentation/pages/new_training/training_division/training_division_controller.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:my_gym_track/src/application/services/locator.dart';
+import 'package:my_gym_track/src/domain/entities/exercise/exercise_entity.dart';
+import 'package:my_gym_track/src/domain/use_cases/get_all_exercises/get_all_exercises_usecase.dart';
+
+enum TrainingDivisionPageState { loading, loaded, error }
+
+class TrainingDivisionController extends ChangeNotifier {
+  final GetAllExercisesUsecase _getAllExercisesUsecase =
+      getIt<GetAllExercisesUsecase>();
+
+  TrainingDivisionPageState _pageState = TrainingDivisionPageState.loading;
+
+  String? errorMessage;
+
+  int selected = 0;
+
+  List<ExerciseEntity> exerciseList = [];
+
+  List<ExerciseEntity> selectedExercises = [];
+
+  TrainingDivisionController() {
+    fetchExercises();
+  }
+
+  TrainingDivisionPageState get pageState => _pageState;
+
+  void fetchExercises({String? muscleGroup}) async {
+    _pageState = TrainingDivisionPageState.loading;
+
+    notifyListeners();
+
+    await Future.delayed(const Duration(seconds: 1), () async {
+      final result =
+          await _getAllExercisesUsecase.call(muscleGroup: muscleGroup);
+      result.fold(
+        (error) {
+          errorMessage = error;
+          _pageState = TrainingDivisionPageState.error;
+        },
+        (exercises) {
+          exercises.sort((a, b) => a.name.compareTo(b.name));
+          exerciseList.clear();
+          exerciseList = exercises;
+
+          _pageState = TrainingDivisionPageState.loaded;
+        },
+      );
+    });
+
+    notifyListeners();
+  }
+
+  void onSelectWorkout(ExerciseEntity exercise) {
+
+    if (exerciseIsAlreadyOnList(exercise.id)) {
+      selectedExercises.removeWhere((element) => element.id == exercise.id);
+    } else {
+      selectedExercises.add(exercise);
+    }
+    notifyListeners();
+  }
+
+  bool exerciseIsAlreadyOnList(String id) =>
+      selectedExercises.any((element) => element.id == id);
+}
